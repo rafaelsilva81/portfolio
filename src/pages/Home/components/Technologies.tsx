51fda0bff5735ba6ca97c93723c7868f8c7db8b7
Rafael Galdino da Silva <rafaelgaldinosilva81@gmail.com>
2022-10-09 15:32:44 -0300

    refactor: minor refactor/small fixes

--- src/pages/Home/components/Technologies.tsx
+++ src/pages/Home/components/Technologies.tsx
@@ -1,5 +1,5 @@
 import React from 'react'
-import { ts, js, python, node, react, firebase, html, css, tailwind, mongo, pgsql, ionic, git, bootstrap, php } from '../../../assets/Technologies'
+import { ts, js, python, node, react, firebase, html, css, tailwind, mongo, pgsql, ionic, git, bootstrap, php } from '../../../assets/TechnologiesList'
 import { motion } from 'framer-motion'
 
 export const Technologies = () => {
@@ -17,7 +17,7 @@ export const Technologies = () => {
                 <div className="flex flex-col">
                     <span className="text-indigo-700 text-3xl font-bold my-2"> {'< Tech Skills />'} </span>
                     <h2 className="text-6xl font-bold text-neutral-200 mb-2 italic"> And what can you work with? </h2>
-                    <p className="text-xl text-neutral-200 mb-4"> Those are the Programming Languages and Technologies I'm currently working with the most:  </p>
+                    <p className="text-xl text-neutral-200 mb-8"> Those are the Programming Languages and Technologies I'm currently working with the most:  </p>
                 </div>
             </motion.div>
 
@@ -25,26 +25,25 @@ export const Technologies = () => {
                 initial="hidden"
                 whileInView="visible"
                 viewport={{ once: true }}
-                transition={{ duration: 0.3 }}
+                transition={{ duration: 0.5 }}
                 variants={{
                     visible: { x: 0, opacity: 1 },
                     hidden: { x: -100, opacity: 0 },
                 }}>
-                <div className="grid grid-cols-3 md:grid-cols-6 gap-4">
+                <div className="grid grid-cols-3 md:grid-cols-12 gap-4">
                     <TechnologyIcon icon={ts} name="Typescript" />
-                    <TechnologyIcon icon={js} name="Javascript" />
                     <TechnologyIcon icon={node} name="NodeJS" bg />
                     <TechnologyIcon icon={react} name="ReactJS" />
                     <TechnologyIcon icon={firebase} name="Firebase" bg />
                     <TechnologyIcon icon={mongo} name="MongoDB" bg />
-                    <TechnologyIcon icon={git} name="Git" bg />
                     <TechnologyIcon icon={tailwind} name="Tailwind" bg />
-                    <TechnologyIcon icon={python} name="Python" bg />
-                    <TechnologyIcon icon={php} name="PHP" bg />
-                    <TechnologyIcon icon={ionic} name="Ionic" bg />
-                    <TechnologyIcon icon={bootstrap} name="Bootstrap" />
                 </div>
             </motion.div>
+            <p className="text-lg text-neutral-200 mt-4"> I used almost all of those for this portfolio! (Except for Firebase).
+                Everything you see here was built in React using Typescript and Tailwind CSS. <br />
+                The projects you're gonna see next were grabbed using an API built in NodeJS (Express) using MongoDB as the database. <br />
+                <strong>  But I know a lot more stuff and I'm always learning something new! </strong> </p>
+
         </div>
     )
 }
